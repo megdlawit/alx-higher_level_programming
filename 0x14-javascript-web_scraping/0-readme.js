f7838cfc3ad0d4c8e2cdf62261fb5934ce2567bd
megdlawit anbessie <megdalenea@gmail.com>
2022-10-18 23:40:32 +0300

    s

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-// Reads and prints the content of a file
 
-const argv = process.argv;
-let fs = require('fs');
-fs.readFile(argv[2], 'utf8', function (err, data) {
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
-    console.log(data);
+    process.stdout.write(data);
   }
 });
