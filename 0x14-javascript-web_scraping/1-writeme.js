009e8f74db15566a248aa30405dff525ca038070
megdlawit anbessie <megdalenea@gmail.com>
2022-10-18 23:38:57 +0300

    d

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
-// Writes a string to a file
 
 const fs = require('fs');
-const argv = process.argv;
-let filePath = argv[2];
-let string = argv[3];
 
-fs.writeFile(filePath, string, 'utf8', function (err) {
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err) {
   if (err) {
     console.log(err);
   }
